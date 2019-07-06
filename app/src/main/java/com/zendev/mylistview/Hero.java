99d44a28d82c27ef0958cae99dc343cf42a41c82
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-07-06 21:35:39 +0700

    Initial Init

---
+++ app/src/main/java/com/zendev/mylistview/Hero.java
@@ -0,0 +1,32 @@
+package com.zendev.mylistview;
+
+class Hero {
+
+    private int photo;
+    private String name;
+    private String description;
+
+    int getPhoto() {
+        return photo;
+    }
+
+    void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+}
